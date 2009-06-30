54fe05edd5ac0033a492cf765bab290760e7266b
ujihisa <ujihisa@gmail.com>
2009-07-01 01:11:06 +0900

    Automatically updated index.html

--- cool.rb
+++ cool.rb
@@ -1,7 +1,8 @@
 @body = `pandoc README.md`
 
 if __FILE__ == $0
-  system "git checkout origin/gh-pages -b gh-pages"
+  #system "git checkout origin/gh-pages -b gh-pages"
+  system "git checkout gh-pages"
   system "git merge master"
   system "erb -r #{__FILE__} -T - -P index.erb > index.html"
   system "git add index.html"
