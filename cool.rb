072fb9eab13c5c18d7b247e120d4258db61a3722
ujihisa <ujihisa@gmail.com>
2009-06-30 10:56:39 +0900

    Added cool.rb to format erb to index

---
+++ cool.rb
@@ -0,0 +1,6 @@
+if __FILE__ == $0
+  system "erb -r #{__FILE__} -T - -P index.erb > index.html"
+  system "git add index.html"
+  system "git commit -m 'Automatically updated index.html'"
+  system "git push"
+end
