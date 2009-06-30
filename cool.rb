b0cf5e333c1437cbc365bc57b8073577aef008ad
ujihisa <ujihisa@gmail.com>
2009-06-30 23:55:05 +0900

    only readme in master

--- cool.rb
+++
@@ -1,8 +0,0 @@
-@body = `pandoc README.md`
-
-if __FILE__ == $0
-  system "erb -r #{__FILE__} -T - -P index.erb > index.html"
-  system "git add index.html"
-  system "git commit -m 'Automatically updated index.html'"
-  system "git push"
-end
