9c5273ecffc227fd967bd7d2d0a2b0e2b9824846
ujihisa <ujihisa@gmail.com>
2009-07-02 17:40:42 +0900

    Fixed

--- cool.rb
+++ cool.rb
@@ -1,11 +1,12 @@
 @body = `pandoc README.md`
 
 if __FILE__ == $0
+  # assume current branch is master
   if false # TODO: If gh-pages isn't exist
-    system "git checkout origin/gh-pages -b gh-pages"
+    system "git branch gh-pages origin/gh-pages"
   else
-    system "git checkout gh-pages"
-  end
+  system "git push"
+  system "git checkout gh-pages"
   system "git merge master"
   system "erb -r #{__FILE__} -T - -P index.erb > index.html"
   system "git add index.html"
