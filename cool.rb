ecf5b9ba884d6a595188c5c53694f7eba22fc2f8
ujihisa <ujihisa@gmail.com>
2009-07-03 17:42:01 +0900

    Fix bug

--- cool.rb
+++ cool.rb
@@ -4,7 +4,7 @@ if __FILE__ == $0
   # assume current branch is master
   if false # TODO: If gh-pages isn't exist
     system "git branch gh-pages origin/gh-pages"
-  else
+  end
   system "git push"
   system "git checkout gh-pages"
   system "git merge master"
