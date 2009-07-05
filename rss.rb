2c2f536a0898945de29570ec80040e3c1e164b60
ujihisa <ujihisa@gmail.com>
2009-07-05 20:45:53 +0900

    Fixed not to use DATA for a library

--- rss.rb
+++ rss.rb
@@ -4,21 +4,7 @@ class RSS
     [:title, :description, :link, :items].each do |x|
       raise unless hash[x]
     end
-    ERB.new(DATA.read).result(binding)
-  end
-end
-
-if $0 == __FILE__
-  hashes = []
-  rss = RSS.make(
-    :title => "Recent Commits to livecoding6:master with Diff",
-    :description => "Recent Commits to livecoding6:master with Diff",
-    :link => "http://ujihisa.github.com/livecoding6/",
-    :items => hashes)
-  puts rss
-end
-
-__END__
+    template = <<EOF # {{{
 <?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0"
   xmlns:content="http://purl.org/rss/1.0/modules/content/"
@@ -40,3 +26,19 @@ __END__
     <% end %>
   </channel>
 </rss>
+EOF
+    # }}}
+    ERB.new(template).result(binding)
+  end
+end
+
+if $0 == __FILE__
+  hashes = []
+  rss = RSS.make(
+    :title => "Recent Commits to livecoding6:master with Diff",
+    :description => "Recent Commits to livecoding6:master with Diff",
+    :link => "http://ujihisa.github.com/livecoding6/",
+    :items => hashes)
+  puts rss
+end
+# vim: foldmethod=marker
